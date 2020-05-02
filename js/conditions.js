ee3af0f7ec8a0083db44900671b5c045a5e5a625
Viktoras <vikter@inbox.lt>
2020-05-02 12:37:16 +0300

    Contitions commit

---
+++ js/conditions.js
@@ -0,0 +1,144 @@
+"use strict"
+
+console.log('-----------------1 uzduotis------------');
+
+
+const uno = 5;
+const dos = 12;
+
+if (uno > dos) {
+    console.log( 'Pomidoras' );   
+} else {
+    console.log( 'Bandyk dar kartą.' );
+}
+
+if (uno < dos) {
+    console.log('Pomidoras');
+    
+}else {
+    console.log('Bandyk dar kartą.');
+    
+}
+
+if (uno === dos) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą.');  
+}
+
+if (uno !== dos) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą.');   
+}
+
+if (uno >= dos) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą.');
+}
+
+if (uno <= dos) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą.');
+}
+
+console.log('-----------------2 uzduotis------------');
+
+
+const A = 'Anubis'; 
+const S=  'Sfinksas'; 
+
+const lengthA = A.length;
+console.log(lengthA);
+
+const lengthS = S.length;
+console.log(lengthS);
+
+console.log('-----------------3 uzduotis------------');
+
+if (A > S) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą.');   
+}
+
+if (A < S) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą.');
+}
+
+if (A === S) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą.');
+}
+
+if (A !== S) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą');
+}
+
+if (A >= S) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą.');
+}
+
+if (A <= S) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą.');
+}
+
+console.log('-----------------4 uzduotis------------');
+
+const vikingai = [5, 2, 6, 8, 7, 12, 15, 24, 3];
+const vikingaiLength = vikingai.length;
+
+const euroJack = [2, 5, 4, 9, 10, 25, ]
+const euroLenght = euroJack.length;
+
+console.log(vikingaiLength);
+console.log(euroLenght);
+
+console.log('-----------------5 uzduotis------------');
+
+if (vikingai > euroJack) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą');
+}
+
+if (vikingai < euroJack) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą');
+}
+
+if (vikingai === euroJack) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą');    
+}
+
+if (vikingai !== euroJack) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą');
+}
+
+if (vikingai >= euroJack) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą');    
+}
+
+if (vikingai <= euroJack) {
+    console.log('Pomidoras');
+} else {
+    console.log('Bandyk dar kartą');    
+}
